77bc926c125bcd47a050491c17a5264e11b695ca
Oliver Sosa <cano@fispan.com>
2022-06-29 10:57:56 -0500

    Minor change to trigger ci

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App({ signOut }: any) {
         <Image src={logo} className="App-logo" alt="logo" />
         <Heading level={1}>We now have Auth!</Heading>
       </Card>
-      <Button onClick={signOut}>Click to sign out</Button>
+      <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
 }
