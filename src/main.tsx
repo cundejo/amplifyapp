9429820b62e16d961d342154da59730b21a6e5e3
Oliver Sosa <cano@fispan.com>
2022-06-29 12:18:39 -0500

    Minor change to trigger ci

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Amplify from 'aws-amplify';
 import App from './App';
-import './config';
 import './index.css';
 // @ts-ignore
 import config from './aws-exports';
