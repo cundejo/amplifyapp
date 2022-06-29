fe84455cc1edc3f54b39cb9f62964a7d7229502a
Oliver Sosa <cano@fispan.com>
2022-06-29 12:13:21 -0500

    Minor change to trigger ci

--- src/config.ts
+++
@@ -1,5 +0,0 @@
-import Amplify from 'aws-amplify';
-// @ts-ignore
-import config from './aws-exports';
-
-Amplify.configure(config);
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './config'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Amplify from 'aws-amplify';
+import App from './App';
+import './config';
+import './index.css';
+// @ts-ignore
+import config from './aws-exports';
+
+Amplify.configure(config);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
